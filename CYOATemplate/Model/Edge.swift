d54fbb2eec23f550068ecb0a0f9ae2710f613a40
Russell Gordon <rgordon@lcs.on.ca>
2023-05-29 13:52:39 -0400

    Got a basic game view created.

--- CYOATemplate/Model/Edge.swift
+++ CYOATemplate/Model/Edge.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Edge {
+struct Edge: Hashable {
     
     // MARK: Stored properties
     let destinationId: Int
--- CYOATemplate/Views/GameView.swift
+++ CYOATemplate/Views/GameView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct GameView: View {
+    
+    // MARK: Stored properties
+    @State var currentNode: Int = 1
+    
+    // MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            
+            ForEach(storyNodes[currentNode].paragraphs, id: \.self) { currentParagraph in
+                
+                Text(currentParagraph)
+                
+            }
+            
+            Divider()
+            
+            ForEach(storyNodes[currentNode].edges, id: \.self) { currentEdge in
+                
+                Text("\(currentEdge.prompt)")
+                    .onTapGesture {
+                        withAnimation {
+                            currentNode = currentEdge.destinationId
+                        }
+                    }
+                
+            }
+            
+            
+        }
+        .padding()
     }
 }
 
