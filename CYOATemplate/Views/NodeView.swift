ce65b09334fc8ef18d202d0ba3de6a4340209f02
Russell Gordon <rgordon@lcs.on.ca>
2023-06-02 12:02:39 -0400

    Now showing the visit count for a node.

--- CYOATemplate/Views/NodeView.swift
+++ CYOATemplate/Views/NodeView.swift
@@ -27,19 +27,30 @@ struct NodeView: View {
     var body: some View {
         if let node = nodes.results.first {
 
-            // Show a Text view, but render Markdown syntax, preserving newline characters
-            Text(try! AttributedString(markdown: node.narrative,
-                                       options: AttributedString.MarkdownParsingOptions(interpretedSyntax:
-                                                                                              .inlineOnlyPreservingWhitespace)))
-            .onAppear {
-                // Update visits count for this node
-                Task {
-                    try await db!.transaction { core in
-                        try core.query("UPDATE Node SET visits = Node.visits + 1 WHERE node_id = ?", currentNodeId)
+            VStack {
+                
+                Divider()
+                
+                Text("Node visited \(node.visits) times.")
+                
+                Divider()
+                
+                
+                // Show a Text view, but render Markdown syntax, preserving newline characters
+                Text(try! AttributedString(markdown: node.narrative,
+                                           options: AttributedString.MarkdownParsingOptions(interpretedSyntax:
+                                                                                                  .inlineOnlyPreservingWhitespace)))
+                .onAppear {
+                    // Update visits count for this node
+                    Task {
+                        try await db!.transaction { core in
+                            try core.query("UPDATE Node SET visits = Node.visits + 1 WHERE node_id = ?", currentNodeId)
+                        }
+                        
                     }
                     
                 }
-                
+
             }
             
         } else {
