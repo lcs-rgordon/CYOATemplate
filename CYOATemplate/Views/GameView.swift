5bfc8d808aaab704a9eb3806bb3e6f70a27a2610
Russell Gordon <rgordon@lcs.on.ca>
2023-06-02 12:25:12 -0400

    Got the number of nodes visited and total nodes displayed in GameView.
    TODO: Figure out why node count is not updating on other nodes.

--- CYOATemplate/Views/GameView.swift
+++ CYOATemplate/Views/GameView.swift
@@ -11,11 +11,37 @@ import SwiftUI
 struct GameView: View {
     
     // MARK: Stored properties
+    
+    // What node are we on?
     @State var currentNodeId: Int = 1
+    
+    // How many nodes have been visited?
+    @BlackbirdLiveQuery(tableName: "Node", { db in
+        try await db.query("SELECT COUNT(*) AS VisitedNodeCount FROM Node WHERE Node.visits > 0")
+    }) var nodesVisitedStats
+
+    // How many nodes have been visited?
+    @BlackbirdLiveQuery(tableName: "Node", { db in
+        try await db.query("SELECT COUNT(*) AS TotalNodeCount FROM Node")
+    }) var totalNodesStats
+    
+    // The actual integer value for how many nodes have been visited
+    var visitedNodes: Int {
+        return nodesVisitedStats.results.first?["VisitedNodeCount"]?.intValue ?? 0
+    }
+
+    // The actual integer value for how many nodes have been visited
+    var totalNodes: Int {
+        return totalNodesStats.results.first?["TotalNodeCount"]?.intValue ?? 0
+    }
     
     // MARK: Computed properties
     var body: some View {
         VStack(spacing: 10) {
+            
+            Text("A total of \(visitedNodes) nodes out of \(totalNodes) nodes overall have been visited in this story.")
+            
+            Divider()
             
             HStack {
                 Text("\(currentNodeId)")
